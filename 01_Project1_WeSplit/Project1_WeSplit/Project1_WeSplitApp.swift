fc1d335a70b5446652fc27d69d17bdd727345e45
Minju Lee <prnbljk@gmail.com>
2021-12-03 15:05:40 +0900

    Day16_Project1

---
+++ 01_Project1_WeSplit/Project1_WeSplit/Project1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1_WeSplitApp.swift
+//  Project1_WeSplit
+//
+//  Created by Minju Lee on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct Project1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
