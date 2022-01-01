d27dc562b4bf4bed0060b3ab13d5a95c1dfd0b54
Minju Lee <prnbljk@gmail.com>
2022-01-02 01:10:05 +0900

    Day30_Project5

--- 07_Project5_WordScramble/Project5_WordScramble/ContentView.swift
+++ 07_Project5_WordScramble/Project5_WordScramble/ContentView.swift
@@ -8,141 +8,145 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
-        /*
-        //정적으로 생성
-        List {
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
-        }
-         */
-        
-        /*
-        //동적으로 생성
-        List {
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
-            }
-        }
-         */
-        
-        /*
-        //동적, 정적 row 같이
-        List {
-            Text("Static row 1")
-            Text("Static row 2")
 
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
-            }
-
-            Text("Static row 3")
-            Text("Static row 4")
-        }
-        */
-        
-        /*
-        //section 사용
-        //.listStyle 설정
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle.fill")
+                            Text(word)
+                        }
+                    }
                 }
             }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+        }
+    }
+    
+    func addNewWord() {
+        // 1. lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        // 소문자로 바꾸고 공백제거
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // 2. exit if the remaining string is empty
+        // 1자 이상인지 확인
+        guard answer.count > 0 else { return }
+        
+        // 3. validation
+        // 유효성 검사
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
 
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
         }
-        .listStyle(.grouped)
-        */
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
-        /*
-        //Form과 다르게 List는 ForEach를 사용하지 않고 동적 행을 생성할 수 있음
-        List(0..<5) {
-            Text("Dynamic row \($0)")
+        // 4. Insert that word at position 0 in the usedWords array
+        // usedWords 배열의 가장 앞에 해당 단어 추가
+        withAnimation {
+            usedWords.insert(answer, at: 0)
         }
-         */
         
-        /*
-        //id 매개변수
-        List(people, id: \.self) {
-            Text($0)
-        }
-         */
-        
-        //+ 동적, 정적 row 같이
-        List {
-            Text("Static Row")
-            
-            ForEach(people, id: \.self) {
-                Text($0)
-            }
-            
-            Text("Static Row")
-        }
+        // 5. Set newWord back to be an empty string
+        //newWord 초기화
+        newWord = ""
     }
     
-    //main 앱 번들에 있는 파일의 URL 읽기
-    func loadFile() {
-        //파일이 존재하지 않을 수 있어서 옵셔널 바인딩
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            //URL이 있으면 문자열 읽기
-            //파일을 찾을 수 없거나 로드할 수 없으면 오류가 발생할 수 있으므로 try
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string!
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        // 번들에서 start.txt 찾기
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            
+            // 2. Load start.txt into a string
+            // 문자열에 로드
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                // 줄바꿈 기준으로 문자열을 나눠서 배열로 만들기
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                // 임의의 단어를 루트단어로 설정하기. 할 수 없다면 silkworm을 기본 값으로 설정
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                // If we are here everything has worked, so we can exit
+                return
             }
         }
+        
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        // url을 찾을 수 없거나(bundle에서 start.txt를 찾을수 없거나), 로드할 수 없는 경우
+        fatalError("Could not load start.txt from bundle.")
     }
     
-    func test() {
-        /*
-        //1) 문자열이 공백으로 분할되어 있는 경우
-        let input = "a b c"
-        //문자열을 분할하여 문자열 배열로 변환
-        let letters = input.components(separatedBy: " ")
-         */
-        
-        /*
-        //2) 문자열이 줄바꿈으로 분할되어 있는 경우
-        let input = """
-a
-b
-c
-"""
-        //문자열을 분할하여 문자열 배열로 변환
-        let letters = input.components(separatedBy: "\n")
-        
-        //랜덤 항목 (배열이 비어있을 수 있으므로 옵셔널)
-        let letter = letters.randomElement()
-        
-        //공백 제거
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-        */
-        
-        
-        //3) 틀린 철자 확인
-        //3-1) 확인 할 단어와 UITextChecker 인스턴스 만들기
-        let word = "swift"
+    // 1) 이미 사용한 단어인지 확인
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    // 2) 사용할 수 있는 단어인지 확인
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+    
+    // 3) 철자가 맞는 단어인지 확인
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-        //3-2) 확인하려는 문자열의 양 알려주기
-        //Objective-C는 모든 문자의 전체 길이를 사용하여 Objective-C 문자열 range를 생성하도록 Swift에 요청해야 합
+        // 확인하려는 문자열의 양 알려주기
+        // Objective-C는 모든 문자의 전체 길이를 사용하여 Objective-C 문자열 range를 생성하도록 Swift에 요청해야 함
         let range = NSRange(location: 0, length: word.utf16.count)
-        //3-3) UITextChecker에서 단어의 철자 오류를 발견한 위치를 보고하도록 요청
+        // UITextChecker에서 단어의 철자 오류를 발견한 위치를 보고하도록 요청
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        //철자 오류가 없으면 (Objective-C는 optional이 없음) NSNotFound를 반환
-        //철자 오류가 없으면 true
-        let allGood = misspelledRange.location == NSNotFound
+
+        // 철자 오류가 없으면 (Objective-C는 optional이 없음) NSNotFound를 반환
+        // 철자 오류가 없으면 true
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
